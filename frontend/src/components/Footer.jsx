dd91a7bf97132036d4adf606b68ae63ee44f209b
Your Name <you@example.com>
2024-08-31 03:26:21 +0100

    Progress...

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <div>
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
-        <div>
-          <img src={assets.logo} alt='Logo' className='mb-5 w-[120px]' />
+        <div className='my-auto'>
+          <img src={assets.logo} alt='Logo' className='mb-4 w-[120px]' />
           <p className='w-full md:2/3 text=[#505050]'>
             Your Fashion, Unveiled
           </p>
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 const Collection = () => {
+  // const { products } = useContext(shop)
+
   return (
     <div>
       
